4901a4b4c67bb0b8b189d987ce52a7e852253d1f
dominikpre <dominik.pre@gmail.com>
2022-04-12 12:23:57 +0200

    Change smart contract and corresponding test to handle multiple in and output tokens

--- contracts/TestRoute.sol
+++ contracts/TestRoute.sol
@@ -6,49 +6,63 @@ import "./interfaces/IWETH9.sol";
 import "./interfaces/IUniswapV2Router02.sol";
 import "./interfaces/IVault.sol";
 
-contract TestRoute {
-    address public immutable WETH;
-    address public immutable DAI;
-    address public immutable yvDAI;
-    address public immutable UniswapV2Router02;
+import "hardhat/console.sol";
 
-    constructor(address _WETH, address _DAI,address _yvDAI address _UniswapV2Router02) {
-        WETH = _WETH;
-        DAI = _DAI;
-        UniswapV2Router02 = _UniswapV2Router02;
+contract YearnZapper {
+    //TODO: Only for debugging purposes. Will be removed when contract can accept any token.
+    address public immutable WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    // address public immutable DAI;
+    // address public immutable YvDAI;
+    //TODO: hardcode router?
+    address public immutable UniswapV2Router02 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+
+    constructor(
+        // address _WETH, 
+        // address _DAI, 
+        // address _YvDAI, 
+        // address _UniswapV2Router02
+        ) {
+        // WETH = _WETH;
+        // DAI = _DAI;
+        // YvDAI = _YvDAI;
+        // UniswapV2Router02 = _UniswapV2Router02;
     }
 
     //TODO: Create mapping of _tokenOut to _tokenOutSwap, so that if e.g., _tokenOut = yvDAI -> _tokenOutSwap = DAI
     function deposit(
-        address _tokenIn,
-        address _tokenOut,
+        address _tokenInUni,
+        address _tokenOutYearn,
         uint256 _amountIn,
         uint256 _maxSlippage
     ) external payable {
-        swap(_tokenIn, _tokenOut, _amountIn, _maxSlippage);
-        // yvDeposit(_tokenOut, );
+        address _tokenInYearn = IVault(_tokenOutYearn).token();
+        swap(_tokenInUni, _tokenInYearn, _amountIn, _maxSlippage);
+        IERC20(_tokenInYearn).approve(_tokenOutYearn, IERC20(_tokenInYearn).balanceOf(address(this)));
+        yvDeposit(_tokenOutYearn, IERC20(_tokenInYearn).balanceOf(address(this)));
     }
 
     function swap(
-        address _tokenIn,
-        address _tokenOut,
+        address _tokenInUni,
+        address _tokenInYearn,
         uint256 _amountIn,
         uint256 _maxSlippage
     ) internal {
         address[] memory path;
-        if (_tokenIn != WETH) {
+    
+        //TODO: Right now contract only accepts WETH. Make it accept any token by checking best route from _tokenInUni to _TokenOutUni
+        if (_tokenInUni != WETH) {
             //TODO: Is msg.value always ETH? Or can other tokens be sent to payable functions?
             require(msg.value == _amountIn, "Wrong amount of ETH sent.");
             path = new address[](3);
-            path[0] = _tokenIn;
+            path[0] = _tokenInUni;
             path[1] = WETH;
-            path[2] = _tokenOut;
+            path[2] = _tokenInYearn;
         } else {
             //TODO: Resolve the double check of _tokenIn == WETH
-            require(_tokenIn == WETH);
+            require(_tokenInUni == WETH);
             path = new address[](2);
             path[0] = WETH;
-            path[1] = _tokenOut;
+            path[1] = _tokenInYearn;
         }
 
         IERC20(WETH).transferFrom(msg.sender, address(this), _amountIn);
@@ -67,17 +81,18 @@ contract TestRoute {
             _amountIn,
             _amountOutMin,
             path,
+            // msg.sender,
+            //TODO: msg.sender only for debugging purposes
             address(this),
             block.timestamp
         );
     }
 
     function yvDeposit(
-        address _tokenIn,
-        address _tokenOut,
+        address _vaultReceiptToken,
         uint256 _amountIn
     ) internal {
-
+        IVault(_vaultReceiptToken).deposit(_amountIn, msg.sender);
     }
 
     receive() external payable {}
--- test/sample-test.js
+++ test/sample-test.js
@@ -3,32 +3,47 @@ const { ethers } = require("hardhat");
 
 const weth9_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const dai_address = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const yvDai_address = "0xdA816459F1AB5631232FE5e97a05BBBb94970c95";
 const uniswapV2Router02_address = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-describe("TestRoute contract", function () {
+describe("YearnZapper contract", function () {
   it("Should have more DAI balance after swapping from WETH", async function () {
     const [owner, addr1, addr2] = await ethers.getSigners();
 
-    const TestRoute = await ethers.getContractFactory("TestRoute");
-    const testRoute = await TestRoute.deploy(weth9_address, dai_address, uniswapV2Router02_address);
-    await testRoute.deployed();
+    const YearnZapper = await ethers.getContractFactory("YearnZapper");
+    const yearnZapper = await YearnZapper.deploy();
+    await yearnZapper.deployed();
 
     let weth9 = await ethers.getContractAt("contracts/interfaces/IWETH9.sol:IWETH9", weth9_address);
     let dai = await ethers.getContractAt("contracts/interfaces/IERC20.sol:IERC20", dai_address);
+    let yvdai_ERC20 = await ethers.getContractAt("contracts/interfaces/IERC20.sol:IERC20", yvDai_address);
 
     let dbb = await dai.balanceOf(owner.address);
     let dbb2 = ethers.utils.formatUnits(dbb, 18);
     let dai_balance_before = ethers.utils.parseUnits(dbb2);
+    console.log("DAI Balance before: " + dai_balance_before);
+
+    let yvdbb = await yvdai_ERC20.balanceOf(owner.address);
+    let yvdbb2 = ethers.utils.formatUnits(yvdbb, 18);
+    let yvdai_balance_before = ethers.utils.parseUnits(yvdbb2);
+    console.log("yvDAI Balance before: " + yvdai_balance_before);
 
     await weth9.deposit({value: ethers.utils.parseEther("1")});
-    await weth9.approve(testRoute.address, ethers.utils.parseEther("1"));
-    const setSwapTx = await testRoute.deposit(weth9_address, dai_address, ethers.utils.parseEther("1"), 1);
+    await weth9.approve(yearnZapper.address, ethers.utils.parseEther("1"));
+    const setSwapTx = await yearnZapper.deposit(weth9_address, yvDai_address, ethers.utils.parseEther("1"), 1);
     await setSwapTx.wait();
 
     let dba = await dai.balanceOf(owner.address);
     let dba2 = ethers.utils.formatUnits(dba, 18);
     let dai_balance_after = ethers.utils.parseUnits(dba2);
+    console.log("DAI Balance after: " + dai_balance_after);
 
-    expect(dai_balance_after).to.above(dai_balance_before);
+    let yvdba = await yvdai_ERC20.balanceOf(owner.address);
+    let yvdba2 = ethers.utils.formatUnits(yvdba, 18);
+    let yvdai_balance_after = ethers.utils.parseUnits(yvdba2);
+    console.log("yvDAI Balance after: " + yvdai_balance_after);
+
+    // expect(dai_balance_after).to.above(dai_balance_before);
+    expect(yvdai_balance_after).to.be.above(yvdai_balance_before);
   });
 });
